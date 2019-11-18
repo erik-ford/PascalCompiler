5e81ca6ba0df8ee6d7711638164f29c786ba5324
erik-ford <erikford28@gmail.com>
2019-11-17 23:41:16 -0600

    Second Delivery

--- GoodSourceCode.pas
+++ GoodSourceCode.pas
@@ -1,11 +1,10 @@
 program example(input,output);
 var x,y:integer;
-if z=1 then x=1
 function gcd(a,b:integer):integer;
 begin{gcd}
 if b=0 then gcd:=a else gcd:=gcd(b,a mod b)
 end;{gcd}
 begin{example}
 read(x,y);
-write(gcd(x,y));
+write(gcd(x,y))
 end.
